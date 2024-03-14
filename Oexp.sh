c7ae53d1f2a1790963f79c7aa7976b4c9a6b3d85
euku <eugeneku123@gmail.com>
2024-03-14 13:40:21 -0700

    cleaning files

--- Oexp.sh
+++
@@ -1,2 +0,0 @@
-nohup python -u noisy_student.py --reparam_mode MLP --student_size 120 > ./logs/Oe2000_0.95lr_MLP.txt &
-nohup python -u noisy_student.py --student_size 120 > ./logs/Oreference_proper.txt &
--- labels_w_scores.py
+++
@@ -1,5 +0,0 @@
-import noisy_student
-
-pLabelsWscore = noisy_student.generate_psuedo_labels()
-
-pLabelsWscore.to_csv('psuedo_labels_w_scores.csv')
